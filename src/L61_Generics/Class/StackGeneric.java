eaa9492b8c921a54c5c3cb5b2a334034c16745f1
Sweta <49400750+HSweta@users.noreply.github.com>
2021-06-15 12:49:10 +0530

    Generics

---
+++ src/L61_Generics/Class/StackGeneric.java
@@ -0,0 +1,66 @@
+package L61_Generics.Class;
+
+public class StackGeneric {
+
+	protected int[] data;
+	protected int tos;
+
+	public StackGeneric() {
+		data = new int[5];
+		tos = -1;
+	}
+
+	public StackGeneric(int cap) {
+		data = new int[cap];
+		tos = -1;
+	}
+
+	public void push(int item) throws Exception {
+		if (isFull()) {
+			throw new Exception("Stack is full");
+		}
+		tos++;
+		data[tos] = item;
+	}
+
+	public int pop() throws Exception {
+		if (isEmpty()) {
+			throw new Exception("Stack is Empty");
+		}
+		int rv = data[tos];
+		data[tos] = 0;
+		tos--;
+
+		return rv;
+	}
+
+	public int peek() throws Exception {
+		if (isEmpty()) {
+			throw new Exception("Stack is Empty");
+		}
+		return data[tos];
+	}
+
+	public int size() {
+		return tos + 1;
+	}
+
+	public boolean isFull() {
+
+		return size() == data.length;
+
+	}
+
+	public boolean isEmpty() {
+		return size() == 0;
+	}
+
+	public void display() {
+		System.out.println("-------------------");
+		for (int i = tos; i >= 0; i--) {
+			System.out.print(data[i] + " ");
+		}
+		System.out.println();
+		System.out.println("-------------------");
+	}
+}
