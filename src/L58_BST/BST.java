607922b56bee44e27edbbe678041e412b7222ec5
Sweta <49400750+HSweta@users.noreply.github.com>
2021-06-15 12:49:22 +0530

    BST

--- src/L58_BST/BST.java
+++ src/L58_BST/BST.java
@@ -69,7 +69,7 @@ public class BST {
 
 	private int max(Node node) {
 
-		if (root.right == null) {
+		if (node.right == null) {
 			return node.data;
 		}
 
@@ -187,6 +187,13 @@ public class BST {
 	}
 
 	public void add2(int item) {
+
+		if (root == null) {
+			Node nn = new Node();
+			nn.data = item;
+			root = nn;
+			return;
+		}
 		add2(root, null, item);
 	}
 
@@ -214,17 +221,18 @@ public class BST {
 	}
 
 	public void add3(int item) {
-		add3(root, item);
+		root = add3(root, item);
 
 	}
 
 	private Node add3(Node node, int item) {
-        
+
 		if (node == null) {
 			Node nn = new Node();
 			nn.data = item;
 			return nn;
-		
+		}
+		// left
 		if (item <= node.data) {
 			node.left = add3(node.left, item);
 		}
@@ -232,7 +240,90 @@ public class BST {
 		else {
 			node.right = add3(node.right, item);
 		}
+		return node;
 
 	}
+
+	public void remove(int item) {
+
+		if (root == null)
+			return;
+
+		if (item == root.data) {
+
+			if (root.left == null && root.right == null) {
+				root = null;
+			}
+
+			if (root.left != null && root.right == null) {
+				root = root.left;
+			} else if (root.left == null && root.right != null) {
+				root = root.right;
+			}
+
+		} else {
+			remove(root, null, item);
+		}
+	}
+
+	private void remove(Node node, Node parent, int item) {
+
+		if (item < node.data) {
+			remove(node.left, node, item);
+		}
+		else if (item > node.data) {
+			remove(node.right, node, item);
+		} 
+		else {
+
+			// leaf node
+			if (node.left == null && node.right == null) {
+
+				// left child
+				if (node.data <=parent.data) {
+					parent.left = null;
+				}
+
+				// right child
+				else {
+					parent.right = null;
+				}
+			}
+
+			else if (node.left == null && node.right != null) {
+
+				if (node.data <= parent.data) {
+					parent.left = node.right;
+				}
+
+				// right child
+				else {
+					parent.right = node.right;
+				}
+			}
+
+			else if (node.left != null && node.right == null) {
+
+				if (node.data <= parent.data) {
+					parent.left = node.left;
+				}
+
+				else {
+					parent.right = node.left;
+				}
+			}
+
+			else {
+				int temp = max(node.left);
+				node.data = temp;
+				remove(node.left, node, temp);
+
+			}
+
+		}
+
+	}
+
+	
 
 }
--- src/L58_BST/BSTClient.java
+++ src/L58_BST/BSTClient.java
@@ -5,12 +5,24 @@ public class BSTClient {
 	public static void main(String[] args) {
 		
 		
-		int[] in = {10,20,30,40,50,60,70};
+		int[] in = {};//{10,20,30,40,50,60,70};
 		BST bst = new BST(in);
-		bst.display();
+		//bst.display();
 		//System.out.println(bst.find(50));
 		//bst.replaceWithSumLarger();
-		bst.add1(35);
+		bst.add3(100);
+		bst.add3(50);
+		bst.add3(200);
+		bst.add3(25);
+		bst.add3(75);
+		bst.add3(12);
+		bst.add3(30);
+		bst.add3(60);
+		bst.add3(80);
+		bst.add3(35);
+		
+		bst.remove(50);
+		
 		bst.display();
 		
 		
